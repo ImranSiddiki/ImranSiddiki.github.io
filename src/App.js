dc143bdcb46484607fc9e012ed021a21ae1786f3
ImranSiddiki <ImranSiddiki78692@gmail.com>
2022-02-27 17:42:35 +0530

    resume

--- src/App.js
+++ src/App.js
@@ -8,6 +8,19 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Imran's Web Site coming soon...</p>
         <p>I'm a Data Analyst enthusiast with a Bachelor of Mathematics</p>
+        <a
+          className="App-Link"
+          style={{
+            textDecoration: "underline",
+            color: "#61dafb",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            window.open("resume.pdf", "_blank");
+          }}
+        >
+          Download my Resume
+        </a>
       </header>
     </div>
   );
